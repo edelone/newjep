c3213248135930c65590611765f25aba993b5010
edelone <edelone@gmail.com>
2014-07-19 16:49:45 -0400

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -7,7 +7,8 @@ then
 	mkdir "$folder"; cd "$folder"
 
 	echo "Downloading game files"
-	for i in {1..4321}
+	a = range(3815, 4567)
+	for i in a
 	do
 		curl -v -o "$i.html" "http://j-archive.com/showgame.php?game_id=$i"
 		sleep 5s # Remember to be kind to the server
